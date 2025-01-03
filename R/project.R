62cdcf843453b4097f8a8e7e2bf41dbbe768cf1a
Nicole Mucci <1639112094113305@mil>
2025-01-03 11:05:21 -0500

    commiting data package

---
+++ R/project.R
@@ -0,0 +1,11 @@
+#' Data of visual detections by Protected Species Observers (PSOs) for the Community Offshore Wind Project, 2023
+#'
+#'
+#'
+#' @format A tibble with 2255 rows and 82 variables:
+#' \describe{
+#'   \item{PROJECT_NUMBER}{Number identifier of wind farm development project}
+#'   \item{VESSEL}{Name of vessel}
+#' }
+#' @source \url{https://www.fisheries.noaa.gov/action/incidental-take-authorization-community-offshore-wind-llc-marine-site-characterization}
+"project"
